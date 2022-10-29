02b27ae41d637e3e5253233ce4e7dde794bf4252
Harper Hsiao <72118443+Harperxx25@users.noreply.github.com>
2022-10-29 19:07:25 +1100

    Make it better.

--- app.js
+++ app.js
@@ -144,6 +144,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, fucntion(){
-  console.log("Server has started successfully!")
-});
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});   
